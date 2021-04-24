341f7b0a9a59bb3f99fc1870404a3f6f053f2de7
mojaeb <mojaebi@gmail.com>
2021-04-24 23:40:46 +0430

    feat: loop in times and fire the notifications

---
+++ src/loop_notifications.rs
@@ -0,0 +1,17 @@
+use std::time::Duration;
+use std::thread::sleep;
+use winrt_notification::{Duration as ToasDuration, Sound, Toast};
+
+
+pub fn navigate(duration: u64) {
+	loop {
+		sleep(Duration::new(duration * 60, 0));
+		Toast::new(Toast::POWERSHELL_APP_ID)
+	        .title("wup")
+	        .text1("wake up now")
+	        .sound(Some(Sound::SMS))
+	        .duration(ToasDuration::Short)
+	        .show()
+	        .expect("unable to toast");
+	}
+}
